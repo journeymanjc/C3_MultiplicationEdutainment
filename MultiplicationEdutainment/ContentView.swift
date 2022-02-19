d5407f106f038d92ac124a1192f46b8b8d74f396
Jae Cho <jjaecho@gmail.com>
2022-02-18 23:24:19 -0500

    Initial Commit

---
+++ MultiplicationEdutainment/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MultiplicationEdutainment
+//
+//  Created by Jae Cho on 2/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MultiplicationEdutainment/MultiplicationEdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationEdutainmentApp.swift
+//  MultiplicationEdutainment
+//
+//  Created by Jae Cho on 2/18/22.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationEdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
